a407a3ce8a7cd90b57c3df5caa2bea8eb105e660
Thomas Bell <thomasjbell065@gmail.com>
2022-06-12 16:34:49 -0400

    updated code
    changed IdentityUser class to BlogUser class and all its references

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -31,8 +31,8 @@ namespace BlogProject.Models
 
         // Navigation Properties
         public virtual Post Post { get; set; }
-        public virtual IdentityUser Author { get; set; }
-        public virtual IdentityUser Moderator { get; set; }
+        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser Moderator { get; set; }
 
 
     }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -15,7 +15,7 @@ namespace BlogProject.Models
         public string Text { get; set; }
 
         public virtual Post Post { get; set; }
-        public virtual IdentityUser Author { get; set; }
+        public virtual BlogUser Author { get; set; }
 
 
     }
